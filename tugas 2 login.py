20c91632a1bfdf27f74501ec39802292b051f8ce
Sanskie19 <muhammadihsanansori2@gmail.com>
2024-11-11 21:00:04 +0700

    Add files via upload

---
+++ tugas 2 login.py
@@ -0,0 +1,26 @@
+#NAMA : MUHAMMAD IHSAN ANSORI
+#NIM : 2400409
+#KELAS : RPL 1C
+
+def menu_login():
+    username = "Daspro2023"
+    password_benar = "Latihan"
+    
+    kesempatan = 3
+    
+    print("Selamat datang di menu login!")
+    print(f"Username: {username}")
+    
+    while kesempatan > 0:
+        password_input = input("Masukkan password: ")
+        
+        if password_input == password_benar:
+            print("Login berhasil! Selamat datang.")
+            return 
+        else:
+            kesempatan -= 1
+            print(f"Password salah. Anda memiliki {kesempatan} kesempatan tersisa.")
+    
+    print("Anda telah memasukkan password yang salah sebanyak 3 kali. Akses ditolak.")
+
+menu_login()
